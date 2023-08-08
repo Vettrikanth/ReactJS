fd2ed9de4a638e400b069e624f23224a47a1326b
vettrikanth <msvettrikanth@gmail.com>
2023-08-08 23:42:24 +0530

    updated

--- vettri/src/Components/Projects.js
+++ vettri/src/Components/Projects.js
@@ -1,6 +1,53 @@
-const Projects=()=>{
-    return(
-        <h1>My projectsaan zsnlas lknd</h1>
+import bg from "./image/bg.jpg"
+
+const Projects = () => {
+    return (
+        <div>
+            <img src={bg} class="bg"></img>
+            <h1>My projects</h1>
+            <p>Elephant Emotion Classification</p>
+            <p>
+                Due to urbanization and increased human activity near forests, human animal interactions are becoming more and more common. Animals like elephants that are unpredictable incur a lot of damage to both life and property.
+                Especially, when elephants go rogue.There are various reasons for why the elephants get into the areas. There are a lot of ways to detect elephants and there are various flaws in them. There are also stealth predators like jaguars that enter
+                into villages and cause damage to both humans and cattle.The elephants that are in trouble inside the forests get timely help only when reported.
+                <p>In 2019-20, 585 deaths due to elephant attack happened, whereas in between 2020-21, almost 461 such casualties were reported and in the last year, the cases gained a marginal hike with almost 535 being reported by several state governments, according to the data provided by the ministry.
+
+                    Among human casualties of conflict with animals, elephants killed 1,579 humans in three years — 585 in 2019-20, 461 in 2020-21, and 533 in 2021-22. Odisha accounted for the highest number of these deaths at 322, followed by Jharkhand at 291 (including 133 in 2021-22 alone), West Bengal at 240, Assam at 229, Chhattisgarh at 183, and Tamil Nadu at 152.
+                </p>
+            </p>
+            <ul className="resume">
+                <p>Exisiting solution</p>
+                <li>Object detection Techniques</li>
+                <li>Beehive Fences</li>
+                <li>
+                    Thorn bush barriers</li>
+                <li>
+                    Deep trenches</li>
+                <li>
+                    Electric Fences
+                </li>
+                <li>
+
+                    Aversive Jio fencing devices</li>
+            </ul>
+
+            <ul className="resume">
+<li>The cameras placed in the trees cannot detect the presence of elephants out of its range.
+</li>
+<li>
+The usage of honey bees as a preventive measure might cause the elephants to run towards the village. </li>
+            <li>
+The voice based detection systems in the existing solution cannot classify the emotion of elephants like if its rogue or mad, asks for help, or in danger etc.</li>
+            <li>One of the major downsides of modern beekeeping is the transmission of disease and parasites.</li>
+            <li> Migratory beekeepers move their hives across the country for pollination and this practice has the potential to spread disease and pests not just among honey bee colonies, but to wild bees as well. 
+The shortage of bees in some cases has increased the distance some beekeepers must move their hives to help farmers pollinate their crops and this compounds the problem
+The Electric fences are illegal.</li>
+            
+            </ul>
+
+
+            {/* <p>Automated helmet detection</p> */}
+        </div>
     )
 }
 export default Projects;
--- vettri/src/Components/Resume.js
+++ vettri/src/Components/Resume.js
@@ -1,13 +1,51 @@
 import './Resume.css';
 import resume from './image/resume.jpg'
 import t from './image/tony.jpeg'
- 
-const Resume=()=>{
-    return(
-        <div>
-            <img src={resume} className='a'></img>
+import bg from "./image/bg.jpg"
+
+const Resume = () => {
+    return (
+        <div className='resume'>
+            <img src={bg} class="bg"></img>
+            <p>I’m a hardworking and passionate engineer interested in the concepts of machine learning and deep learning.I am focused on gaining practical knowledge so as to enhance my skills and I’m looking for an opportunity to showcase my skills.</p>
+
+            <ol>
+                <p>Education</p>
+                <li>Bala Matric Higher Secondary School 	95.8%</li>
+                <li>Bala Matric Higher Secondary School 	96.87%</li>
+                <li>B.Tech Artificial Intelligence and Data Science 3rd year  CGPA:8.85</li>
+            </ol>
+            <ol>
+                <p> skills</p>
+                <li>
+                    Leadership
+                </li>
+                <li>Content Writing </li>
+                <li>Team Management
+                </li>
+                <li>Problem Solving</li>
+            </ol>
+            <ul>
+                <p>PROGRAMMING LANGUAGES & SKILLS</p>
+                <li>Programming: Python</li>
+                <li>ML: Scikit learn</li>
+                <li>Data Analysis:
+                    <ul>
+                        <li>Excel,  </li>
+                        <li> pandas,</li>
+                        <li>numpy ,</li>
+                        <li> matplot </li>
+                        <li>,seaborn</li>
+                    </ul> </li>
+            </ul>
+            <ul>
+                <p>Co-Curricular & Extra Curricular Activities:</p>
+                <li>Media and Marketing Coordinator in Department Association</li>
+                <li>Active Volunteer in NSS</li>
+            </ul>
+
         </div>
-        
+
     )
 }
 export default Resume;
